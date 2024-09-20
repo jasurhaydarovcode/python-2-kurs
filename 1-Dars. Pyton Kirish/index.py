3c1d0f30145c4199a2120047bc12a32e89315ec9
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-09-20 16:12:09 +0500

    First Lesson

---
+++ 1-Dars. Pyton Kirish/index.py
@@ -0,0 +1,49 @@
+import math
+
+# a = 2 
+# b = 8
+# print(a**b)
+
+
+
+# c = "Hello!"
+# print(c * 10)
+
+
+
+# a = input("a ni kiriting ")
+# b = input("b ni kiriting ")
+
+
+
+# a = int (input("A ni kiriting "))
+# b = int (input("B ni kiriting "))
+# print((a * b)-10)
+
+
+
+# d = bool (9)
+# e = bool ()
+# print(d, e)
+
+
+
+# a = int (input("A = "))
+# b = int (input("b = "))
+# c = ((a + b) + b)
+# print(c)
+
+
+# a = 'Hello'
+# b = 'Guys'
+# print(a, '\t', b)
+
+
+# son = int (input("√ = "))
+# ildiz = math.sqrt(son)
+# print(ildiz)
+
+
+
+# x = math.log10(64)
+# print(x)
